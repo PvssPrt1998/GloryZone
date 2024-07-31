4c3d1451e682a8b39a4bcdbd80175525ad8b9001
WebShell1998 <43918790+WebShell1998@users.noreply.github.com>
2024-07-31 14:22:25 +0500

    Initial Commit

---
+++ ProScore/ProScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProScoreApp.swift
+//  ProScore
+//
+//  Created by Николай Щербаков on 31.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ProScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
